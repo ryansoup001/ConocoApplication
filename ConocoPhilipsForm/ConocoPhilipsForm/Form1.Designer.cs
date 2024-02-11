4d9b8a48f86b770723424502451f10f904671fd7
Ruben Ramirez <rj@allvalleyservicesca.com>
2024-02-10 20:18:48 -0600

    Added 4 items. country and co2

--- ConocoPhilipsForm/ConocoPhilipsForm/Form1.Designer.cs
+++ ConocoPhilipsForm/ConocoPhilipsForm/Form1.Designer.cs
@@ -29,24 +29,68 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(243, 42);
+            label1.Location = new Point(451, 90);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(47, 15);
+            label1.Size = new Size(97, 32);
             label1.TabIndex = 0;
             label1.Text = "Testing!";
             label1.Click += label1_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 168);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Pick one";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(66, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 32);
+            label2.TabIndex = 3;
+            label2.Text = "Country";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(66, 275);
+            label3.Name = "label3";
+            label3.Size = new Size(159, 32);
+            label3.TabIndex = 4;
+            label3.Text = "CO2 Emission";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(66, 348);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "0";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1486, 960);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
+            Margin = new Padding(6, 6, 6, 6);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -56,5 +100,9 @@
         #endregion
 
         private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
     }
 }
