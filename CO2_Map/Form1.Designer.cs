0541c4a3da5df87f7b78c87c9a9835c02b415b95
Ruben Ramirez <rj@allvalleyservicesca.com>
2024-02-10 19:56:35 -0600

    1st commit. Added Global Map view

---
+++ CO2_Map/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CO2_Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pix1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pix1).BeginInit();
+            SuspendLayout();
+            // 
+            // pix1
+            // 
+            pix1.Image = Properties.Resources.conocoGlobe;
+            pix1.Location = new Point(-2, 2);
+            pix1.Name = "pix1";
+            pix1.Size = new Size(1872, 879);
+            pix1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pix1.TabIndex = 0;
+            pix1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1877, 882);
+            Controls.Add(pix1);
+            Name = "Form1";
+            Text = "ConocoPhillips Global Map";
+            ((System.ComponentModel.ISupportInitialize)pix1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pix1;
+    }
+}
---
+++ CO2_Map/Form1.cs
@@ -0,0 +1,10 @@
+namespace CO2_Map
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
