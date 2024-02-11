321ddf6a3a104ec630b155a374c07ba770509d8d
Ruben Ramirez <rj@allvalleyservicesca.com>
2024-02-10 20:10:12 -0600

    1st push after ryan_branch conflicts

--- ConocoPhilipsForm/ConocoPhilipsForm/obj/Debug/net8.0-windows/ConocoPhilipsForm.AssemblyInfo.cs
+++ ConocoPhilipsForm/ConocoPhilipsForm/obj/Debug/net8.0-windows/ConocoPhilipsForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConocoPhilipsForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6cd5345f8c9142f1dbd732b4cb73b62da2fec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConocoPhilipsForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConocoPhilipsForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
