001561fcc83a062ce6df6b200ebfe5acbfa20f31
Ruben Ramirez <rj@allvalleyservicesca.com>
2024-02-10 20:32:22 -0600

    Form2 adding global map with Ryan form

---
+++ ConocoPhilipsForm/ConocoPhilipsForm/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ConocoPhilipsForm
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pix1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pix1).BeginInit();
+            SuspendLayout();
+            // 
+            // pix1
+            // 
+            pix1.Image = Properties.Resources.conocoGlobe;
+            pix1.Location = new Point(2, 0);
+            pix1.Name = "pix1";
+            pix1.Size = new Size(1737, 818);
+            pix1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pix1.TabIndex = 0;
+            pix1.TabStop = false;
+            pix1.Click += pix1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1747, 823);
+            Controls.Add(pix1);
+            Name = "Form2";
+            Text = "ConocoPhillips Global Map";
+            ((System.ComponentModel.ISupportInitialize)pix1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pix1;
+    }
+}
